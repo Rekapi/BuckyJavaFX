29681f21116aeaa330cfb5275e22177f49dbc240
mahmoud <m.rekapi@gmail.com>
2019-05-10 11:44:55 +0200

    The first tutorials

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,38 @@ import javafx.stage.Stage;
 // making window with single button which printing out "Hello Lambda Exp"
 public class Main extends Application {
     Button button;
+    Stage window;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello FX");
+         window = primaryStage;
+        window.setTitle("Hello FX");
+        // closing method
+        window.setOnCloseRequest(event -> {
+            event.consume();
+            closeProgram();
+        });
         button = new Button();
+        Button button2 = new Button("Close the window ");
         button.setText("Click me");
         button.setOnAction(event -> {
             System.out.println("Hello Lambda Exp"); // to make multilines
+            // confirmBox
+           boolean result =  ConfirmBox.display("Title of the window", "Are you sure do this ");
+            System.out.println(result);
         });
+        button2.setOnAction(event -> closeProgram());
+
         StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        layout.getChildren().addAll(button,button2);
         Scene scene = new Scene(layout);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void closeProgram(){
+        Boolean answer = ConfirmBox.display("title "," Are sure you want to close ");
+        if(answer)
+            window.close();
     }
 
     public static void main(String[] args) {
