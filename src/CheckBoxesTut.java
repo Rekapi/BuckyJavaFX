eb98224a839069f1cf98c3d2d388f9b6d0ed7797
mahmoud <m.rekapi@gmail.com>
2019-05-10 11:43:16 +0200

    implementation of CheckBoxes

--- src/CheckBoxesTut.java
+++ src/CheckBoxesTut.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -13,13 +14,30 @@ public class CheckBoxesTut extends Application {
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
         window.setTitle("Title");
-        button = new Button("Click me");
+
+        // Declaring CheckBoxes
+        CheckBox box1 = new CheckBox("Bacon");
+        CheckBox box2 = new CheckBox("Tuna");
+        box2.setSelected(true);  // make it default checked
+
+        button = new Button(" Order now");
+        button.setOnAction(event -> handleOptions(box1,box2));
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(button);
+        layout.getChildren().addAll(box1,box2,button); // arranging your components the first will appear first and so on ...
         Scene scene = new Scene(layout,300,250);
         window.setScene(scene);
         window.show();
+    }
+    // handle option method
+    private void handleOptions(CheckBox box1, CheckBox box2){
+        String message = "user order : ";
+        if(box1.isSelected())
+            message += "Bacon\n";
+
+        if(box2.isSelected())
+            message +="Tuna\n";
+        System.out.print(message);
     }
 
     public static void main(String[] args) {
